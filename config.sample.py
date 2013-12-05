26a5bce7c4eb0e249edb9fe2294a675088ee10e9
Alojzij Blatnik <lojze.blatnik@gmail.com>
2013-12-06 00:36:05 +0100

    Call data from trac ticket by xmlrpc

--- config.sample.py
+++ config.sample.py
@@ -1,21 +1,11 @@
-# That's credentials which will be used for parse from ticket description from trac project
+# If username and password for basic authentication contains special characters
+# use %__ where _ is number represented in uri eg. %40 for @
+web_uri= "http://user:pass@domain.tld/login/xmlrpc"
 
-# Username and password is used for basic authentication
-# Provide basic encoded username and password
-# You will get it with:
-#import base64
-#print base64.b64encode("John")
-#print base64.b64encode("Smith")
-
-# Example. If username is "John" and password is "Smith" following entry is correct:
-username='Sm9obg=='
-password='U21pdGg='
-
-# Example URL to web page
-web_location="http://your.web.page.tld/ticket/100"
+web_ticket_num=123
 
 # file will be used if you'll set parameter use_web=False
-file_location="example_work_data.dat"
+file_location="data.dat"
 
 # use file or data retreived from web
 use_web=False
--- parser.py
+++ parser.py
@@ -1,134 +1,157 @@
 # -*- coding:utf8 -*-
 import config
-import urllib2,base64,re,time,datetime,sys
+import urllib2, base64, re, time, datetime, sys, xmlrpclib, codecs
 
-
-def get_data(file_loc="",web_loc="",username="",password=""):
-    """
-    If you decited to read from file just file_loc="<file location>" needed
-    If you want to read from web trac ticket you need to give web_loc and username and password for basic authentication
-    """
-    if web_loc!="":
-        request = urllib2.Request(web_loc)
-        base64string = base64.encodestring('%s:%s' % (username, password)).replace('\n', '')
-        request.add_header("Authorization", "Basic %s" % base64string)   
-        result = urllib2.urlopen(request)
-        result= result.read()
+class TicketParser:
+    def get_data_from_web(self):
+        """
+        install plugin to trac installation (see http://trac-hacks.org/wiki/XmlRpcPlugin)
+        ssh to server
+        easy_install -Z -U http://trac-hacks.org/svn/xmlrpcplugin/trunk
         
-        adding = 0
-        description = []
-        for line in result.split("\n"):
-            if adding == 1 and '</textarea>' in line:
-                break
-            if adding == 1:
-                description.append(line)
-            if '<textarea id="field-description" name="field_description"' in line:
-                adding = 1
-        return description
-
-    elif file_loc!="":
+        add this to trac.ini
+            [components] 
+            tracrpc.* = enabled 
+        """
+        server = xmlrpclib.ServerProxy(config.web_uri)
+        ticket = server.ticket.get(config.web_ticket_num)
+        comments = server.ticket.changeLog(config.web_ticket_num)
+        
+        description = ticket[3]["description"]
+        comments_content = ""
+        for comment in comments:
+            if comment[2] == "comment":
+                comments_content += comment[4] + "\n"
+        
+        return (description + "\n" + comments_content).split("\n")
+        
+    def get_data_from_file(self):
+        """
+        get data from file_location specified in config.py (default data.dat)
+        """
         try:
-            return file(file_loc,"rb").read().split("\n")
+            return file(config.file_location,"rb").read().split("\n")
         except IOError,e:
             print e
-    else:
-        raise ValueError, "No location or web address given"
-
-def parser(data):
-    """
-    :parm data: list of records
-    """
-    days = []
-    skipped_days = []
-
-    for line in data:
-        stripped_line = line.strip()
-
-        if stripped_line == "":
-            # skip empty line
-            continue
-        if re.match("^[0-9]{1,2}.[0-9]{1,2}$", stripped_line):
-            # add new day
-            # append tuple as example ('30.10', [ ... day entries ... ])
-            days.append(tuple([stripped_line,[]]))
-        elif re.match("^[0-9]{1,2}:[0-9]{2}[ ]*-[ ]*[0-9]{1,2}:[0-9]{2}[ ]*=[ ]*.*$", stripped_line):
-            # add new day entry
-            try:
-                days[-1][1].append(stripped_line)
-            except: skipped_days.append(stripped_line) 
+    
+    def parser(self, data):
+        """
+        :parm data: list of records
+        """
+        days = []
+        skipped_days = []
+        year = "1900"
+    
+        for line in data:
+            stripped_line = line.strip()
+    
+            if stripped_line == "":
+                # skip empty line
+                continue
+            
+            if re.match("^year:[0-9]{4}$", stripped_line):
+                year = stripped_line.split(":")[1]
+            elif re.match("^[0-9]{1,2}.[0-9]{1,2}$", stripped_line):
+                # add new day
+                # append tuple as example ('30.10', '1900', [ ... day entries ... ])
+                days.append(tuple([stripped_line, year, []]))
+            elif re.match("^[0-9]{1,2}:[0-9]{2}[ ]*-[ ]*[0-9]{1,2}:[0-9]{2}[ ]*=[ ]*.*$", stripped_line):
+                # add new day entry
+                try:
+                    days[-1][2].append(stripped_line)
+                except: skipped_days.append(stripped_line) 
+            else:
+                # unparsable line
+                skipped_days.append(stripped_line)
+        return days,skipped_days
+    
+    def mock_date(self, date,year,start,end,ret=""):
+        if int(time.mktime(time.strptime(start, "%H:%M"))) < int(time.mktime(time.strptime(end, "%H:%M"))):
+            return date
         else:
-            # unparsable line
-            skipped_days.append(stripped_line)
-    return days,skipped_days
-
-def mock_date(date,year,start,end,ret=""):
-    if int(time.mktime(time.strptime(start, "%H:%M"))) < int(time.mktime(time.strptime(end, "%H:%M"))):
-        return date
-    else:
-        return (datetime.datetime.fromtimestamp(int(time.mktime(time.strptime(date+"."+year, "%d.%m.%Y"))))+datetime.timedelta(days=1)).strftime("%d.%m")
-
-def time_calculator(days):
-    year = "2012"
-    ndays = []
-    for date, records in days:
-        for record in records:
-            start_end, comment = record.split("=",1)
-            start = start_end.split("-")[0].strip()
-            end = start_end.split("-")[1].strip()
-            # comment structure:
-            # comment
-            # or
-            # comment + money entry
-            # or
-            # comment + money entry + real comment
-            # or
-            # comment + real comment
-            if "//" in comment:
-                comment, real_comment = comment.rsplit("//", 1)
-            else:
-                real_comment = ""
-            
-            if "!#" in comment:
-                comment, money_part = comment.rsplit("!#", 1)
-            else:
-                money_part = ""
-            
-            # strip all tree
-            comment, money_part, real_comment = comment.strip(), money_part.strip(), real_comment.strip()
-            
-            start_sec = int(time.mktime(time.strptime(date+"."+year+" "+start, "%d.%m.%Y %H:%M")))
-            end_sec = int(time.mktime(time.strptime(mock_date(date,year,start,end)+"."+year+" "+end, "%d.%m.%Y %H:%M")))
-
-            str_diff = format_seconds(end_sec-start_sec)
-            ndays.append({"year":year,"date":date,"start":start,"end":end,"duration":(end_sec-start_sec),"str_diff":str_diff,
-                                                        "comment":comment, "money_part":money_part, "real_comment":real_comment, "start_sec":start_sec})
-    return ndays
-
-def format_seconds(sec):
-    if sec < 60:
-        return str(sec)+" s"
-    elif sec < 3600:
-        return str(sec/60)+"m"
-    elif sec < 86400:
-        return str(sec/60/60)+"h "+ str((sec/60)%60)+"m"
-    else:
-        return str(sec/60/60/24)+"d "+str((sec/60/60)%24)+"h "+ str((sec/60)%60)+"m"+"=="+ str(sec/60/60)+"h "+ str((sec/60)%60)+"m"
-
-def find_tags(comment):
-    return [ x for x in comment.split() if re.match("^#[a-zA-Z]+$",x)]
-
-def selected_records(all_times, regex="" ,tags=[]):
-    return [x for x in all_times if (regex=="" or re.match("^.*"+regex+".*$",x["comment"])) and (tags==[] or all((y in find_tags(x["comment"])) for y in tags))]
-
-def print_selected(records,overall_time=False):
-    formated_records = "\n".join([x["date"]+"."+x["year"]+"\t"+x["start"]+"-"+x["end"]+"\t"+x["str_diff"]+"\t"+x["comment"] for x in records])
-    all_time = sum(x["duration"] for x in records)
-    return formated_records+"\nOverall: "+format_seconds(all_time)
-
-def export_to_excel_selected(records,overall_time=False, skip_tags=True):
-    formated_records = "\n".join([x["date"].replace(",",".")+"."+"\t"+x["start"]+"\t"+x["end"]+"\t"+x["str_diff"]+"\t"+" ".join(word for word in x["comment"].split(" ") if not word.startswith("#")).strip() for x in records])
-    file("out.xls","wb").write(formated_records)
-    return
+            return (datetime.datetime.fromtimestamp(int(time.mktime(time.strptime(date+"."+year, "%d.%m.%Y"))))+datetime.timedelta(days=1)).strftime("%d.%m")
+    
+    def time_calculator(self, days):
+        ndays = []
+        for date, year, records in days:
+            for record in records:
+                start_end, comment = record.split("=",1)
+                start = start_end.split("-")[0].strip()
+                end = start_end.split("-")[1].strip()
+                # comment structure:
+                # comment
+                # or
+                # comment + money entry
+                # or
+                # comment + money entry + real comment
+                # or
+                # comment + real comment
+                if "//" in comment:
+                    comment, real_comment = comment.rsplit("//", 1)
+                else:
+                    real_comment = ""
+                
+                if "!#" in comment:
+                    comment, money_part = comment.rsplit("!#", 1)
+                else:
+                    money_part = ""
+                
+                # strip all tree
+                comment, money_part, real_comment = comment.strip(), money_part.strip(), real_comment.strip()
+                
+                start_sec = int(time.mktime(time.strptime(date+"."+year+" "+start, "%d.%m.%Y %H:%M")))
+                end_sec = int(time.mktime(time.strptime(self.mock_date(date,year,start,end)+"."+year+" "+end, "%d.%m.%Y %H:%M")))
+    
+                str_diff = self.format_seconds(end_sec-start_sec)
+                ndays.append({"year":year,"date":date,"start":start,"end":end,"duration":(end_sec-start_sec),"str_diff":str_diff,
+                                                            "comment":comment, "money_part":money_part, "real_comment":real_comment, "start_sec":start_sec})
+        return ndays
+    
+    def format_seconds(self, sec):
+        if sec < 60:
+            return str(sec)+" s"
+        elif sec < 3600:
+            return str(sec/60)+"m"
+        elif sec < 86400:
+            return str(sec/60/60)+"h "+ str((sec/60)%60)+"m"
+        else:
+            return str(sec/60/60/24)+"d "+str((sec/60/60)%24)+"h "+ str((sec/60)%60)+"m"+"=="+ str(sec/60/60)+"h "+ str((sec/60)%60)+"m"
+    
+    def find_tags(self, comment):
+        return [ x for x in comment.split() if re.match("^#[a-zA-Z]+$",x)]
+    
+    def selected_records(self, all_times, regex="" ,tags=[]):
+        return [x for x in all_times if (regex=="" or re.match("^.*"+regex+".*$",x["comment"])) and (tags==[] or all((y in self.find_tags(x["comment"])) for y in tags))]
+    
+    def print_selected(self, records,overall_time=False):
+        formated_records = "\n".join([x["date"]+"."+x["year"]+"\t"+x["start"]+"-"+x["end"]+"\t"+x["str_diff"]+"\t"+x["comment"] for x in records])
+        all_time = sum(x["duration"] for x in records)
+        return formated_records+"\nOverall: "+self.format_seconds(all_time)
+    
+    def export_to_excel_selected(self, records,overall_time=False, skip_tags=True):
+        formated_records = "\n".join([x["date"].replace(",",".")+"."+"\t"+x["start"]+"\t"+x["end"]+"\t"+x["str_diff"]+"\t"+" ".join(word for word in x["comment"].split(" ") if not word.startswith("#")).strip() for x in records])
+        codecs.open("out.xls","wb", encoding="utf-8").write(formated_records)
+        return
+    def print_usage(self, additional=""): # TODO do ith with argparse TODO update README
+        sys.stderr.write(
+            ("" if additional=="" else additional+"\n")
+            +"Usage: parser.py \"tags separated with spaces\" \"regex\"\n"
+            +"Examples:\n"
+            +"Given tags and regex (note: regex is actually ^.*bugfix.*$)\n"
+            +"python parser.py \"#home #python #urllib\" \"bugfix\"\n"
+            +"Ommited tags\n"
+            +"python parser.py \"\" \"bugfix\"\n"
+            +"Ommited regex\n"
+            +"python parser.py \"#home #python #urllib\" \"\"\n")
+        sys.exit(1)
+    
+    def parse_input(self):
+        if len(sys.argv) == 4:
+            return self.find_tags(sys.argv[1]),sys.argv[2],True
+        elif len(sys.argv) == 3:
+            return self.find_tags(sys.argv[1]),sys.argv[2],False
+        else:
+            self.print_usage("You must provide two or three arguments! Given %d"% (len(sys.argv)-1))
 
 class MoneyParser:
     def __init__(self):
@@ -319,11 +342,13 @@ class MoneyParser:
         day=""
         month=""
         newday=""
+        
         # go over time entries
         for time_entry in all_times:
-            moneypart = time_entry["money_part"].decode("utf-8")
+            moneypart = time_entry["money_part"]
+            
             # skip entries which hasn't money entry
-            if moneypart == "": continue
+            if moneypart == u"": continue
             
             # use time stamp from start time of time entry
             time_dt = datetime.datetime.fromtimestamp(time_entry["start_sec"])
@@ -360,52 +385,37 @@ class MoneyParser:
         
         return ret_str
     
-def print_usage(additional=""):
-    sys.stderr.write(
-        ("" if additional=="" else additional+"\n")
-        +"Usage: parser.py \"tags separated with spaces\" \"regex\"\n"
-        +"Examples:\n"
-        +"Given tags and regex (note: regex is actually ^.*bugfix.*$)\n"
-        +"python parser.py \"#home #python #urllib\" \"bugfix\"\n"
-        +"Ommited tags\n"
-        +"python parser.py \"\" \"bugfix\"\n"
-        +"Ommited regex\n"
-        +"python parser.py \"#home #python #urllib\" \"\"\n")
-    sys.exit(1)
-
-def parse_input():
-    if len(sys.argv) == 4:
-        return find_tags(sys.argv[1]),sys.argv[2],True
-    elif len(sys.argv) == 3:
-        return find_tags(sys.argv[1]),sys.argv[2],False
+if __name__ == "__main__":
+    tp = TicketParser()
+    
+    # provide list with records
+    if config.use_web==True:
+        data = tp.get_data_from_web()
     else:
-        print_usage("You must provide two or three arguments! Given %d"% (len(sys.argv)-1))
-# provide list with records
-if config.use_web==True:
-    data = get_data(web_loc=config.web_location,username=base64.b64decode(config.username),password=base64.b64decode(config.password))
-else:
-    data = get_data(file_loc=config.file_location)
-
-# get input (list of tags and string regex)
-tags,regex,money = parse_input()
-
-# structured data
-time_pairs,skipped = parser(data)
-
-# even more structured data
-all_times = time_calculator(time_pairs)
-
-# select records which match all tags AND regex
-selected = selected_records(all_times,regex=regex,tags=tags)
-
-# format for print and add overall spent time
-out = print_selected(selected)
-export_to_excel_selected(selected)
-print out
-
-if money:
-    # money part
-    mp = MoneyParser()
-    print mp.print_money_entries(all_times)
+        data = tp.get_data_from_file()
+    
+    
+    # get input (list of tags and string regex)
+    tags,regex,money = tp.parse_input()
+    
+    # structured data
+    time_pairs,skipped = tp.parser(data)
+    
+    # even more structured data
+    all_times = tp.time_calculator(time_pairs)
+    
+    # select records which match all tags AND regex
+    selected = tp.selected_records(all_times,regex=regex,tags=tags)
+    
+    # format for print and add overall spent time
+    out = tp.print_selected(selected)
+    
+    tp.export_to_excel_selected(selected)
+    print out
+    
+    if money:
+        # money part
+        mp = MoneyParser()
+        print mp.print_money_entries(all_times)
 
 
